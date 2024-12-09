455bc0f9a53eb0d18edff4cacc2d0b37ca3146f0
tanmayrane18 <tanmayrane51@gmail.com>
2024-12-09 23:09:38 +0530

    updated the heading and subheading of the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,16 +15,16 @@ export default function Home() {
             <Image src={"/hero.png"} alt="" fill className=" object-contain" />
        </div>
        {/* TEXT CONTAINER */}
-       <div className=" lg:w-1/2 flex flex-col gap-8 items-center justify-center ">
+       <div className=" lg:w-1/2 flex flex-col gap-8 items-center lg:items-start justify-center ">
             {/* TITLE */}
-            <h1 className=" text-4xl md:text-6xl font-bold">
-                Elevating User Experiences, Shaping Tomorrow&apos;s Web.
+            <h1 className=" text-4xl md:text-6xl font-bold text-left">
+            Building <br className=" hidden lg:block"/> the Future of <br className=" hidden lg:block"/> User Interfaces.
             </h1>
             <p className=" md:text-xl">
-            Step into the realm of innovation where code meets creativity. As a MERN stack developer, I merge cutting-edge technology with captivating design to craft seamless digital experiences. Explore my portfolio and witness the future of web development unfold.
+            Specializing in React & React Native Development to Create Engaging and Intuitive User Interfaces for Web and Mobile Applications.
             </p>
             {/* BUTTONS */}
-            <div className=" flex gap-4 ">
+            <div className=" flex gap-4 self-center">
                 <Link href={"/portfolio"} className=" p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</Link>
                 <Link href={"/contact"} className=" p-4 rounded-lg ring-1 ring-black">Contact Me</Link>
             </div>
