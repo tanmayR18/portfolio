c2dc2c0501cc8b946654515ace989854bb715e31
tanmayrane18 <tanmayrane51@gmail.com>
2024-12-11 23:31:46 +0530

    updaded bio

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -194,7 +194,7 @@ const AboutPage = () => {
                   </div>
                   {/* JOB DESC */}
                   <div className="p-3 text-sm italic">
-                    Managing Websites and Working on addition and modification of features in app @ archstore
+                    Managing Websites and Working on addition and modification of features in app @ archstore.
                   </div>
                   {/* JOB DATE */}
                   <div className="p-3 text-red-400 text-sm font-semibold">
@@ -236,7 +236,7 @@ const AboutPage = () => {
                   </div>
                   {/* JOB DESC */}
                   <div className="p-3 text-sm italic">
-                    Adding and modifing features on websites, building dealer side app @ archstore
+                    Adding and modifing features on websites, building dealer side app @ archstore.
                   </div>
                   {/* JOB DATE */}
                   <div className="p-3 text-red-400 text-sm font-semibold">
